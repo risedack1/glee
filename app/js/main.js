d7db511907f52f288e7cb6f9f47e39910e7b9d36
risedack1 <risedack1@gmail.com>
2021-05-30 22:33:48 +0300

    added product-promo and design section

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,13 @@ $(function(){
         fade: true
     });
 
-    var mixer = mixitup('.products__content');
+    let containerEl1 = document.querySelector('.design');
+    let containerEl2 = document.querySelector('.products');
+    let config = {
+        controls: {
+            scope: 'local',
+        },
+    };
+    (() => mixitup(containerEl1, config))();
+    (() => mixitup(containerEl2, config))();
 });
