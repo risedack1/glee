c7a85b532c3cc52490fca8451a984aab58ecbd83
risedack1 <risedack1@gmail.com>
2021-06-05 16:00:58 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ $(function(){
         arrows: false,
         fade: true
     });
+    $('.partners__inner').slick({
+        arrows: false,
+        slidesToShow: 5
+    });
+
+    let containerEl1 = document.querySelector('.design');
+    let containerEl2 = document.querySelector('.products');
+    let config = {
+        controls: {
+            scope: 'local',
+        },
+    };
+    (() => mixitup(containerEl1, config))();
+    (() => mixitup(containerEl2, config))();
 });
