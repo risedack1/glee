296db735e41801afac18c476653e630e225bd0d1
risedack1 <risedack1@gmail.com>
2021-06-28 20:39:13 +0300

    added filter-shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,74 +1,87 @@
-$(function(){
-    $('.burger').on('click', function(){
-        $('.menu__list').toggleClass('menu__list--active')
-        $('.burger').toggleClass('burger--active')
-    });
+$(function () {
+  $('.burger').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+    $('.burger').toggleClass('burger--active')
+  });
 
-    $('.footer__title--slide').on('click', function(){
-        $(this).next().slideToggle()
-        $(this).toggleClass('footer__title--active')
-    });
-    $('.slider-top__wrapper').slick({
-        dots: true,
-        arrows: false,
-        fade: true
-    });
-    $('.partners__inner').slick({
-        arrows: false,
-        slidesToShow: 5,
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2
-              }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                  slidesToShow: 1
-                }
-              }
-          ]
-    });
+  $('.footer__title--slide').on('click', function () {
+    $(this).next().slideToggle()
+    $(this).toggleClass('footer__title--active')
+  });
+  $('.slider-top__wrapper').slick({
+    dots: true,
+    arrows: false,
+    fade: true
+  });
 
-    $(window).scroll(function () {
-        // sticky navbar on scroll script
-        if (this.scrollY >50) {
-          $('.header').addClass("header--sticky");
-          $('.slider-top').addClass("slider-top--sticky");
-        } else {
-          $('.header').removeClass("header--sticky");
-          $('.slider-top--sticky').removeClass("slider-top--sticky");
+  $(".price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.price__from').text(data.from);
+      $('.price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.price__from').text(data.from);
+      $('.price__to').text(data.to);
+    },
+  });
+
+  $('.partners__inner').slick({
+    arrows: false,
+    slidesToShow: 5,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
         }
-        var scrolled = $(window).scrollTop();
- 
-	      if ( scrolled > 100 && scrolled > scrollPrev ) {
-		      $('.header').addClass('header--visible');
-	      } else {
-		      $('.header').removeClass('header--visible');
-	      }
-	      scrollPrev = scrolled;
-      });
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-      $(window).scroll(function () {
-        $('.menu__list').removeClass('menu__list--active')
-        $('.burger').removeClass('burger--active')
-      });
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 50) {
+      $('.header').addClass("header--sticky");
+      $('.slider-top').addClass("slider-top--sticky");
+    } else {
+      $('.header').removeClass("header--sticky");
+      $('.slider-top--sticky').removeClass("slider-top--sticky");
+    }
+    var scrolled = $(window).scrollTop();
 
-    let containerEl1 = document.querySelector('.design');
-    let containerEl2 = document.querySelector('.products');
-    let config = {
-        controls: {
-            scope: 'local',
-        },
-    };
-    (() => mixitup(containerEl1, config))();
-    (() => mixitup(containerEl2, config))();
+    if (scrolled > 100 && scrolled > scrollPrev) {
+      $('.header').addClass('header--visible');
+    } else {
+      $('.header').removeClass('header--visible');
+    }
+    scrollPrev = scrolled;
+  });
+
+  $(window).scroll(function () {
+    $('.menu__list').removeClass('menu__list--active')
+    $('.burger').removeClass('burger--active')
+  });
+
+  let containerEl1 = document.querySelector('.design');
+  let containerEl2 = document.querySelector('.products');
+  let config = {
+    controls: {
+      scope: 'local',
+    },
+  };
+  (() => mixitup(containerEl1, config))();
+  (() => mixitup(containerEl2, config))();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,7 @@ function scripts () {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'app/js/main.js'
     ])
