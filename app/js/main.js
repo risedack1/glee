22f00e0eb0ef18b8bd0fc73ee275f6f549a2d9e4
risedack1 <risedack1@gmail.com>
2021-07-25 17:01:55 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function () {
-  $('.filter-button').on('click', function() {
+
+  $('.tabs__link').on('click', function (e) {
+    e.preventDefault();
+    $('.tabs__link').removeClass('tabs__link--active');
+    $('.tabs__content').removeClass('tabs__content--active');
+    $(this).addClass('tabs__link--active');
+    $($(this).attr('href')).addClass('tabs__content--active');
+  });
+
+  $('.product-self__input').styler();
+
+
+  $('.filter-button').on('click', function () {
     $('.filter-button').toggleClass('filter-button--active')
     $('.shop-filter').toggleClass('shop-filter--active')
 
@@ -11,7 +23,7 @@ $(function () {
     // })
   });
 
-  $('.shop__btn--list').on('click', function () { 
+  $('.shop__btn--list').on('click', function () {
     $('.product-shop__content').addClass('product-shop__content--active')
     $('.shop__btn--list').addClass('shop__btn--active')
     $('.shop__btn--grid').removeClass('shop__btn--active')
@@ -59,7 +71,34 @@ $(function () {
   $(".star").rateYo({
     rating: 4,
     spacing: "8px",
-    starSvg: "<svg role='img' viewBox='0 0 576 512'><path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path></svg>" 
+    starSvg: "<svg role='img' viewBox='0 0 576 512'><path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path></svg>"
+  });
+
+  $('.related-products__slider').slick ({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: "<button type='button' class='related-products__slick-prev'><svg><use xlink:href='images/sprite.svg#arrow-left'></use></svg></button>",
+    nextArrow: "<button type='button' class='related-products__slick-next'><svg><use xlink:href='images/sprite.svg#arrow-right'></use></svg></button>",
+    infinite: false,
+  });
+
+  $('.slider-small').slick({
+    asNavFor: '.slider-big',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    draggable: false,
+    focusOnSelect: true,
+  });
+
+  $('.slider-big').slick({
+    asNavFor: '.slider-small',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    draggable: false,
+    fade: true,
   });
 
   $('.partners__inner').slick({
@@ -102,7 +141,7 @@ $(function () {
 
     if (this.scrollY > shopHeight) {
       $('.filter-button').addClass('filter-button--disable')
-    }else{
+    } else {
       $('.filter-button').removeClass('filter-button--disable')
     }
 
